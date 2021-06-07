dc96b95fa1b468a52e4cec77a8361ad29ca2d08c
Viacheslav <slavs.pets@gmail.com>
2021-06-07 17:29:28 +0300

    Made it more beautiful and finished

--- HW_2/hw2.py
+++ HW_2/hw2.py
@@ -14,8 +14,6 @@ ROOT_URLCONF=__name__
 DEBUG=True
 SECRET_KEY='qazwsx'
 
-
-
 template = '''
 <!DOCTYPE html>
 <html>
@@ -27,63 +25,50 @@ template = '''
 </body>
 </html>
 '''
+
 template1 = '''
 <!DOCTYPE html>
 <html>
 <head>
  <title></title>
 </head>
-<body>
- <pre style="word-wrap: break-word; white-space: pre-wrap;">{ottr_doc}</pre>
-</body>
+<body>{ottr_doc}</body>
 </html>
 '''
 
-module_doc = list()
-
-def creating_links(module_name):
+def fun_module_doc(request, module_name):
 	try:
 		module = import_module(module_name)
 	except ModuleNotFoundError:
-		HttpResponseNotFound(f'No module named "{module_name}"')
-
+		return HttpResponseNotFound(f'No module named "{module_name}"')
+	long_str_with_links = ""
+	list_with_links = []
+	module_doc = []
 	# filter the module	
 	for attr in dir(module):
 		if attr[0] == "_":
 			pass
 		else:
 			module_doc.append(attr)
-
     # creating links
-	long_str_with_links = ""
-	list_with_links = []
-
 	for i in range(0, len(module_doc)):
 		str_with_link = f'<a href = "{module_name}/{module_doc[i]}">{module_doc[i]}</a><br>'
 		list_with_links.append(str_with_link)
 	long_str_with_links = "".join(list_with_links)
-	return long_str_with_links
-
-def ottrs_doc(module_name, name):
-	
-	try:
-		module = import_module(module_name)
-	except ModuleNotFoundError:
-		HttpResponseNotFound(f'No module named "{module_name}"')
-
-	try:
-		ottr = getattr(module, name)
-	except AttributeError:
-		HttpResponseNotFound(f'module "{module_name}" has no attribute "{name}"')
-
-	return ottr.__doc__
-
-def fun_module_doc(request, module_name):
-	return HttpResponse(template.format(title=f'index of {module_name}', module_ottrs=creating_links(module_name)))								  			
+	return HttpResponse(template.format(title=f'index of {module_name}', module_ottrs=long_str_with_links))								  			
 		
 
 def fun_obj_doc(request, module_name, name):
-	return HttpResponse(template1.format(ottr_doc=ottrs_doc(module_name, name)))
+	try:
+		module = import_module(module_name)
+	except ModuleNotFoundError:
+		return HttpResponseNotFound(f'No module named "{module_name}"')
+	try:
+		ottr = getattr(module, name)
+	except AttributeError:
+		return HttpResponseNotFound(f'module "{module_name}" has no attribute "{name}"')
+	return HttpResponse(template1.format(ottr_doc=f'<pre style="word-wrap: break-word; white-space: pre-wrap;">{ottr.__doc__}</pre>'))
+	
 
 urlpatterns = [
 	path("doc/<module_name>", fun_module_doc),
