ae35017ee68154f652f947119d789580a36eab3e
Viacheslav <slavs.pets@gmail.com>
2021-06-22 16:30:37 +0300

    finish

--- HW_4/hw4.py
+++ HW_4/hw4.py
@@ -37,21 +37,14 @@ def almost_all_the_work(request):
 	key = ''
 	if request.POST:
 		link = request.POST.get('url', '')
-		query = 'SELECT key FROM hw4_table_with_key_and_link WHERE link = %s'
-		with connection.cursor() as cur:
-			cur.execute(query, [link])
-			key = cur.fetchone()
-		if key:
-			return render(request, 'template_hw4.html', {'key': key[0]})
+		if link.startswith(('http://', 'https://', 'ftp://')):
+			key = ''.join(choice(''.join([ascii_letters, digits])) for _ in range(5))
+			with connection.cursor() as cur:
+				insert = 'INSERT INTO hw4_table_with_key_and_link (key, link) VALUES (%s, %s)'
+				cur.execute(insert, [key, link])
+			return render(request, 'template_hw4.html', {'key': key})
 		else:
-			if link.startswith(('http://', 'https://', 'ftp://')):
-				key = ''.join(choice(''.join([ascii_letters, digits])) for _ in range(5))
-				with connection.cursor() as cur:
-					insert = 'INSERT INTO hw4_table_with_key_and_link (key, link) VALUES (%s, %s)'
-					cur.execute(insert, [key, link])
-				return render(request, 'template_hw4.html', {'key': key})
-			else:
-				return render(request, 'template_hw4.html', {'others': f'Invalid URL {link}. Allowed schemes: http://,ftp://,https://'})
+			return render(request, 'template_hw4.html', {'others': f'Invalid URL {link}. Allowed schemes: http://,ftp://,https://'})
 	else:
 		return render(request, 'template_hw4.html', {})
 
