83d71977395ad5513df3ce98429b20381310520d
Viacheslav <slavs.pets@gmail.com>
2021-08-08 19:10:57 +0300

    there`s still a problem with UnicodeDecode

--- HW_5/hw5.py
+++ HW_5/hw5.py
@@ -5,49 +5,53 @@ from math import ceil
 
 
 def simple_field(type_, field):
-	def _inner(date):
-		for val in date:
+	def _inner(data):
+		for val in data:
 			type_(val)
 		return field
 	return _inner
 
 
-def char_field(date):
-	max_lenght = cail(max(map(len, date)) * 1,25/10) * 10
+def char_field(data):
+	max_lenght = cail(max(map(len, dataf)) * 1,25/10) * 10
 	return f'char_field(max_lenght={max_lenght})'
 
 
-def gen_field(name, date):
+def gen_field(name, data):
 	validator = [simple_field(int, 'IntegerField()'), simple_field(float, 'FloatField()'), simple_field(UUID, 'UUIDField()'), char_field]
 	for v in validator:
 		try:
-			field = v(date)
+			field = v(data)
 			break
 		except:
 			pass
 	indent = ' ' * 4
-	print(f'{indent}{name} = model.{field}')
+	try:
+		print(f'{indent}{name} = model.{field}')
+	except:
+		print('Yours .csv file cant be read correctly. Please try another file')
+		
 
 
-def gen_module(fname, col_date):
+def gen_model(fname, col_data):
 	print(f'class {fname}(model.Model):')
-	for k, v in col_date.items():
+	for k, v in col_data.items():
 		gen_field(k, v)
 
 
 def process_file(fname):
 	with open(fname) as csvfile:
 		reader = csv.DictReader(csvfile)
-		col_date = defaultdict(set)
+		col_data = defaultdict(set)
 		for row in reader:
 			for k, v in row.items():
-				col_date[k].add(v)
-	return col_date
+				col_data[k].add(v)
+	return col_data
 
 
 if __name__ == '__main__':
 	args = sys.argv[1:]
 	if args:
-		gen_module(args[0], process_file(args[0]))
+		gen_model(args[0], process_file(args[0]))
 	else:
-		print('try again')
+		print('please, enter the file')
