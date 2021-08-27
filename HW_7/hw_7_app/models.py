d3c09309d65ee437fde8b99d2510c39b41f53d87
Viacheslav <slavs.pets@gmail.com>
2021-08-27 22:46:42 +0300

    try to fix PostUpdate.get_object

--- HW_7/hw_7_app/models.py
+++ HW_7/hw_7_app/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
-
+from django.urls import reverse
 
 
 class Post(models.Model):
-	craated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+	created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='post')
 	title = models.CharField(max_length=100)
 	text = models.TextField()
 	slug = models.SlugField(max_length=100, unique=True)
 	created_at = models.DateTimeField(auto_now=True)
 
 	
+	def get_absolute_url(self):
+		return reverse('home')
+
 
 	def __str__(self):
 		return self.title
 
 
 	def save(self, *args, **kwargs):
-		if not self.pk:
+		if not self.slug:
 			self.slug = slugify(self.title)
 		super().save(*args, **kwargs)
 
--- HW_7/hw_7_app/views.py
+++ HW_7/hw_7_app/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseNotFound
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django import forms
@@ -6,26 +8,42 @@ from django.shortcuts import render, redirect
 from .models import Post
 
 
+class PublicPostList(ListView):
+    model = Post
+    template_name = 'Public.html'
+
+
 class PostList(LoginRequiredMixin, ListView):
-	model = Post
-	template_name = 'post_list.html'
+    model = Post
+    template_name = 'post_list.html'
 
 
 class PostDetail(DetailView):
-	model = Post
-	template_name = 'post_detail.html'
+    model = Post
+    template_name = 'post_detail.html'
 
 
 class PostCreate(LoginRequiredMixin, CreateView):
-	model = Post
-	template_name = 'post_create.html'
-	fields = ('title', 'text',)
+    model = Post
+    template_name = 'post_create.html'
+    fields = ('title', 'text',)
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
 
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
-	model = Post
-	template_name = 'post_update.html'
-	fields = ('title', 'text',)
+    model = Post
+    template_name = 'post_update.html'
+    fields = ('title', 'text',)
+    pk_url_kwarg = 'id'
+
+    def get_object(self, **kwargs):
+        post = super().get_object(self, **kwargs)
+        if not post.created_by == self.request.user:
+            raise PermissionDenied
+        return post
 
 
 def create_user(request):
@@ -34,5 +52,3 @@ def create_user(request):
         form.save()
         return redirect('home')
     return render(request, 'register.html', {'form': form})
-
-
